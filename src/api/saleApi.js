97fbcd084b9655711d2e0225d1b97543589e57c3
avezedroos <154615446+avezedroos@users.noreply.github.com>
2026-05-06 13:54:26 +0530

    api updated

--- src/api/saleApi.js
+++ src/api/saleApi.js
@@ -1,9 +1,10 @@
 // src/api/saleApi.js
 import axios from "axios";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
-const BASE_URL = "http://localhost:5000/api/sales";
+// const BASE_URL = "http://localhost:5000/api/sales";
 
 export const getAllSales = async () => {
-  const res = await axios.get(`${BASE_URL}/`);
+  const res = await axios.get(`${BASE_URL}/api/sales/`);
   return res.data;
 };
